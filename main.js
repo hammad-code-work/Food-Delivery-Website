5ea449c8a85363ec03d357a4bcd18e5493f8333d
Hammad Gulzar <hammadgulzar146@gmail.com>
2025-12-02 15:17:34 +0500

    Review & make comments

--- main.js
+++ main.js
@@ -26,22 +26,33 @@ cartTab.classList: This accesses the list of CSS classes currently applied to th
 /*We have to show our Data/cards on screen/website through javascript so we have to fetch data from our jason file we already create*/
 let productList = []; /*a variable and empty array is here, so we will fillup this array with productjason data*/
 const showCards = () => {/*After getting data in array we will show cards*/
- productList.forEach(product => {
-  const orderCard = document.createElement('div');  /*making element in java script*/
-          orderCard.classList.add('order-card') /*this is class*/
-          orderCard.innerHTML = `
+  productList.forEach(product => {
+    const orderCard = document.createElement('div');  /*making element in java script*/
+    orderCard.classList.add('order-card') /*this is class*/
+    orderCard.innerHTML = `
                       <div class="card-image">
                             <img src="${product.image}"> 
                         </div>
                         <h4>${product.name}</h4> 
                         <h4 class="price">${product.price}</h4>
-                        <a href="#" class="btn">Add to Cart</a>
+                        <a href="#" class="btn card-btn">Add to Cart</a> /*(2)after this we are gonna add list in card so we have already give btn too so much classes we will give them different name here*/
                         `;
-/*Append here*/
-cardList.appendChild(orderCard)
+    /*Append here*/
+    cardList.appendChild(orderCard)
+    const cardBtn = orderCard.querySelector('.card-btn') /*select=(1) means cardbtn is in the order card (2)select the class="card-btn" here so CardBtn is constant here*/
+    cardBtn.addEventListener('click', (e)=>{            /*event=after select here  is an event*/
+      e.preventDefault(); /*prevent from extra loading*/
+      addTCart();                     /*call the function= after click this will perfom*/
+    });                           
+  });
+};
+const addToCart = () =>{           /*Functin=during click, this function wil perfom*/
+ const cartItem = document.createElement('div')         /*(imp)go to index.html, see cart-list, then create items of this card via javascript(1) create element first, make div*/
+cartItem.classList.add('item');          /*give div a class name like item(is it in index), so finally we create div with the class*/
+                                   /*after creating parent div, we copy all data*/
 
-})
-}
+} 
+
 
 const initApp = () => {  /*this is arrow function so 1st we fetch data here in the function */
   fetch('products.json').then /*fetch data in function & .then for chaining*/
@@ -51,8 +62,8 @@ const initApp = () => {  /*this is arrow function so 1st we fetch data here in t
       productList = data;
       /*console.log(productList); *//*for checking array is working or not from google inspect*/
       showCards();
-        
-         
-        })
-      }
+
+
+    })
+}
 initApp();                      /*we call arrow function here*/
